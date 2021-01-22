505fb74690edd9bb5a1bb789b667257bd4c54291
galathinius <anisoara.ionela@gmail.com>
2021-01-22 16:43:34 +0200

    initial react front

---
+++ front/src/first.js
@@ -0,0 +1,41 @@
+const exercises = [
+  [
+    {
+      x: 'd',
+      y: '2',
+      c: 'w',
+      q: false
+    },
+    {
+      x: 'f',
+      y: '2',
+      c: 'w',
+      q: false
+    },
+    {
+      x: 'g',
+      y: '1',
+      c: 'w',
+      q: false
+    },
+
+    {
+      x: 'd',
+      y: '4',
+      c: 'b',
+      q: false
+    },
+    {
+      x: 'f',
+      y: '4',
+      c: 'b',
+      q: false
+    },
+    {
+      x: 'f',
+      y: '6',
+      c: 'b',
+      q: false
+    }
+  ]
+]
