3eeae723983da398e3473ed386654b112017e575
galathinius <anisoara.ionela@gmail.com>
2021-01-28 00:26:27 +0200

    style update

--- front/src/App.js
+++ front/src/App.js
@@ -6,9 +6,6 @@ import { Board } from './components/game/Board'
 function App () {
   return (
     <div className='App'>
-      {/* <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-      </header> */}
       <Board />
     </div>
   )
